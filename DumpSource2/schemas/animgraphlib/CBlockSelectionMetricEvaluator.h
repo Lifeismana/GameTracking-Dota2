663d3545841432193e16dc18522d0f1433c04fc0
Wendy <freeman@steamdb.info>
2026-03-11 22:07:31 +0000

    6722 | 29 files | M DumpSource2/schemas/animgraphlib/CBlockSelectionMetricEvaluator.h,  M game/dota/bin/linuxsteamrt64/libserver_strings.txt,  M game/dota/pak01_dir.txt,  M game/dota/steam.inf,  M game/dota_addons/npx_2019/readonly_tools_asset_info.txt,  M game/dota_addons/npx_2019/resource/addon_english.txt,  M game/dota_addons/npx_2019/scripts/npc/npc_items_custom.txt,  M game/dota_addons/npx_2019/scripts/vscripts/addon_game_mode.lua,  M game/dota_addons/npx_2019/scripts/vscripts/npx_scenario.lua,  M game/dota_addons/npx_2019/scripts/vscripts/precache.lua,  M game/dota_addons/npx_2019/scripts/vscripts/scenarios/initiation_teamfight.lua,  M game/dota_addons/npx_2019/scripts/vscripts/scenarios/lockdown_single_target.lua,  M game/dota_addons/npx_2019/scripts/vscripts/scenarios/scenario_basics_intro.lua,  M game/dota_addons/npx_2019/scripts/vscripts/scenarios/scenario_bkb_pa.lua,  M game/dota_addons/npx_2019/scripts/vscripts/scenarios/scenario_creep_pulling.lua,  M game/dota_addons/npx_2019/scripts/vscripts/scenarios/scenario_cr
    https://steamdb.info/patchnotes/22302843/

--- DumpSource2/schemas/animgraphlib/CBlockSelectionMetricEvaluator.h
+++ DumpSource2/schemas/animgraphlib/CBlockSelectionMetricEvaluator.h
@@ -2,11 +2,11 @@
 //	"_class": "CBlockSelectionMetricEvaluator",
 //	"m_means":
 //	[
-//		-30453641879070411819868830086201344.000000
+//		0.000006
 //	],
 //	"m_standardDeviations":
 //	[
-//		-30453721107232926084206423630151680.000000
+//		0.000006
 //	],
 //	"m_flWeight": 0.000000,
 //	"m_nDimensionStartIndex": -1
